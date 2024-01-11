0cba58d97500e52f38991ca26618a34878fc3144
Anders Revsgaard <anders@itfoxtec.com>
2024-01-11 14:00:45 +0100

    Api1

---
+++ ANUG/OidcAndNemLogin/Api1/Models/IdentitySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Api1.Models
+{
+    public class IdentitySettings
+    {
+        public string FoxIDsAuthority { get; set; }
+        public string ResourceId => DownParty;
+        public string DownParty { get; set; }
+    }
+}
