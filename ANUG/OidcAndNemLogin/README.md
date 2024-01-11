be8c5ba5626dd309399a231568d8072b39bda497
Anders Revsgaard <anders@itfoxtec.com>
2024-01-11 15:48:47 +0100

    OIDC

---
+++ ANUG/OidcAndNemLogin/README.md
@@ -0,0 +1,13 @@
+# OpenID Connect and NemLog-in sample
+
+Sample for the ANUG talk: OpenID Connect and NemLog-in – introduction and code.
+
+**WebApp endpoints**
+
+- Login redirect: `https://localhost:44308/signin-oidc`
+- Post logout redirect: `https://localhost:44308/signout-callback-oidc`
+- Front channel logout: `https://localhost:44308/auth/frontchannellogout`
+
+**API1 endpoint**
+
+- Weather API: `https://localhost:44351/WeatherForecast`
--- ANUG/OidcAndNemLogin/WebApp/Controllers/AuthController.cs
+++ ANUG/OidcAndNemLogin/WebApp/Controllers/AuthController.cs
@@ -1,7 +1,24 @@
-﻿namespace WebApp.Controllers
-{
-    public class AuthController
-    {
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
 
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            var redirectUrl = Url.Action(nameof(HomeController.Secure), "Home");
+            return Challenge(new AuthenticationProperties { RedirectUri = redirectUrl }, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            var callbackUrl = Url.Action(nameof(HomeController.Index), "Home", values: null, protocol: Request.Scheme);
+            return SignOut(new AuthenticationProperties { RedirectUri = callbackUrl },
+                CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+        }
     }
 }
